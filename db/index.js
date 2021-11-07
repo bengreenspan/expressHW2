29de5add6c7691ab23347abab9d5ad4bc970a787
bengreenspan <91162661+bengreenspan@users.noreply.github.com>
2021-11-07 10:48:14 -0500

    params

---
+++ db/index.js
@@ -0,0 +1,58 @@
+const pg = require('pg');
+// const postBank = require('./postBank');
+
+const client = new pg.Client('postgres://localhost/sandwichdb2');
+
+const syncAndSeed = async() => {
+const SQL = `
+
+DROP TABLE IF EXISTS Ingred;
+DROP TABLE IF EXISTS Sandwich;
+
+CREATE TABLE Sandwich (
+id INTEGER PRIMARY KEY,
+name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Ingred(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    Sandwich_id INTEGER REFERENCES Sandwich (id)
+    );
+
+INSERT INTO Sandwich(id, name)VALUES(1, 'Buffalo Chicken Sandwich');
+INSERT INTO Sandwich(id, name)VALUES(2, 'Liverwurst Sandwich');
+INSERT INTO Sandwich(id, name)VALUES(3, 'Turkey Club');
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (1, 'Blue Cheese', 1);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (2, 'Buffalo Sauce', 1);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (3, 'Liverwurst', 2);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (4, 'Mustard', 2);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (5, 'Turkey', 3);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (6, 'Bacon', 3);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (7, 'Mayonnaise', 3);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (8, 'Onion', 1);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (9, 'Onion', 2);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (10, 'Chicken', 1);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (11, 'Chicken', 3);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (12, 'Lettuce', 1);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (13, 'Lettuce', 3);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (14, 'Tomato', 1);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (15, 'Tomato', 3);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (16, 'Bread', 1);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (17, 'Bread', 2);
+INSERT INTO Ingred(id, name, sandwich_id) VALUES (18, 'Bread', 3);
+
+`;
+
+
+await client.query(SQL)
+;};
+
+module.exports = {
+    client,
+    syncAndSeed
+};
+
+
+
+
--- server.js
+++ server.js
@@ -1,83 +1,137 @@
+
+const { client, syncAndSeed } = require('./db');
 const express = require("express");
 const morgan = require('morgan');
 const app = express();
-const postBank = require('./postBank');
 const path = require('path');
 
 
+const setUp = async () => {
+  try {
+    await client.connect();
+    await syncAndSeed();
+    console.log('connected to database');
+    app.listen(PORT, () => {
+      console.log(`App listening in port ${1234}`);
+    });
+  }
+  catch(ex){
+    console.log(ex);
+  }
+};
+
+setUp()
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-const posts = postBank.list();
-res.send(
-    `<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Sandwiches</title>
-    <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-    <div class="news-list">
-      <header><img src="/logo.png"/>Sandwiches & Ingredients</header>
-      ${posts.map(post => `
-        <div class='news-item'>
-          <p>
-          <ul>
-          <li>
-           <a href='/${post.id}'> <span class="news-position">${post.title} </span></a>
-           </li>
-           </ul>
-          </p>
-        </div>`
-      ).join('')}
-    </div>
-  </body>
-</html>`
-);
-});
-
-app.get('/:id', (req, res)=> {
-    const id = req.params.id;
-    const post = postBank.find(id);
+app.get('/', async(req, res, next) => {
+  try {
+    const response = await client.query('SELECT * From sandwich;');
+    const sandwiches = response.rows;
     res.send(
-        `<!DOCTYPE html>
+      `<!DOCTYPE html>
       <html>
       <head>
-        <title>Ingredients</title>
+        <title>Sandwiches</title>
         <link rel="stylesheet" href="/style.css" />
-        <nav class='nav-link'>
-  <a href='/'> Back</a>
-  </nav>
       </head>
       <body>
         <div class="news-list">
-          <header><img src="/logo.png"/>${post.title}</header>
-    
-         
-
-
-          <p>
-              <a class="upper">${ post.content }</a>
-              <div class= "sand">
-              <img src="/${post.pic}.png">
-              </div>
-              <ul> 
-              <a class="lower">${ post.Ingredients } </a>
-              </ul>
+          <header><img src="/logo.png"/>Sandwiches</header>
+          ${sandwiches.map(sandwiches => `
+            <div class='news-item'>
+              <p>
+              <ul>
+              <li>
+               <a href='/sandwiches/${sandwiches.name}'> <span class="news-position">${sandwiches.name} </span></a>
+               </li>
+               </ul>
               </p>
-            </div>
+            </div>`
+          ).join('')}
         </div>
       </body>
     </html>`
+
     );
+  }
+  catch(ex){
+    next(ex);
+  }
 });
 
-const PORT = 1234;
+app.get('/sandwiches/:id', async(req, res, next) => {
+  try {
+    let response = await client.query('SELECT * From sandwich where id=$1;', [req.params.id]);
+    const sandwiches = response.rows[0];
+    response = await client.query('SELECT * From ingred where sandwich_id=$1;', [req.params.id]);
+    const ingreds = response.rows;
+    res.send(
+      `<!DOCTYPE html>
+      <html>
+      <head>
+        <title>Sandwiches</title>
+        <link rel="stylesheet" href="/style.css" />
+      </head>
+      <body>
+        <div class="news-list">
+          <header><img src="/logo.png"/><a href='/'>Sandwiches</a></header>
+          <h2>${ sandwiches.name } </h2>
+        </div>
+         <ul>
+         ${
+          ingreds.map( ingred => `
+          <li>
+          ${ ingred.name }
+          </li>
+          `).join('')
+        }
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${1234}`);
+         </ul>
+      </body>
+    </html>
+
+    `);
+  }
+  catch(ex){
+    next(ex);
+  }
 });
+
+app.get( '*', async(req, res, next) => {
+  try {
+    res.send(
+      `<!DOCTYPE html>
+      <html>
+      <head>
+        <title>Sandwiches</title>
+        <link rel="stylesheet" href="/style.css" />
+      </head>
+      <body>
+        <div class="news-list">
+          <header><img src="/logo.png"/>Sandwiches</header>
+     
+     <h1> Page Not Found. </h1>
+
+      <img src="/sad_sandwhich.png"/ width="250" height="200">
+      <h3>The page you requested does not exist. Click <a href='/'>here</a> to continue browsing. <h3?
+    
+        </div>
+      
+      </body>
+    </html>
+
+    `);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+
+const PORT = process.env.PORT || 1234
+
+
 
 
